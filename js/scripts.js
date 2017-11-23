03018fc8c024c27908d7e350a7e637f67d2f37c9
Grzegorz Koza <kozagrzegorz@interia.pl>
2017-11-23 16:44:09 +0100

    Add prompt and view

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
-var a = 5;
-var h = 8;
-var triangleArea = a*h/2;
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+	var a = 5;
+	var h = 8;
+	var triangleArea = (a*h/2);
+	//console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+	document.getElementById("wynik").innerHTML = ('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+
 
 // triangle area with prompt
+
 var valuea = prompt('Write value a');
 var valueh = prompt('Write value h');
-var triangleArea = valuea*valueh/2;
-console.log(' Triangle Area is: ' + triangleArea);
+var triangleArea1 = (valuea*valueh/2);
+//console.log('Triangle Area is: ' + triangleArea1);
+document.getElementById("wynik2").innerHTML = (' Triangle Area is: ' + triangleArea1);
