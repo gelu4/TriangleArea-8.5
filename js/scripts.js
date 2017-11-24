c18f23a986d2198e4333515c4805d7c72d574b68
Grzegorz Koza <kozagrzegorz@interia.pl>
2017-11-24 21:15:07 +0100

    Add two input

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,36 @@
 
-	var a = 5;
-	var h = 8;
-	var triangleArea = (a*h/2);
-	//console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-	document.getElementById("wynik").innerHTML = ('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-
-
+var a = 5;
+var h = 8;
+var triangleArea = (a*h/2);
+document.getElementById("score").innerHTML = 'Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea;
 
 // triangle area with prompt
 
-var valuea = prompt('Write value a');
-var valueh = prompt('Write value h');
-var triangleArea1 = (valuea*valueh/2);
-//console.log('Triangle Area is: ' + triangleArea1);
-document.getElementById("wynik2").innerHTML = (' Triangle Area is: ' + triangleArea1);
+
+	var valuea = prompt('Enter the base value');
+	var valueh = prompt('Enter the height value');	
+	/*var valuea = document.getElementById('value1').value;
+	var valueh = document.getElementById('value2').value;*/	
+	var triangleArea1 = (valuea*valueh/2);
+
+	if ((isNaN(valuea)) || (isNaN(valueh))) {
+	document.getElementById("score2").innerHTML = 'Musisz wpisać liczby';
+	}
+
+	else {
+	document.getElementById("score2").innerHTML = ' Triangle Area is: ' + triangleArea1; 
+	}
+
+
+// triangle area with two number area
+function area2() {
+	var valuea = document.getElementById('value1').value;
+	var valueh = document.getElementById('value2').value;	
+	var triangleArea1 = (valuea*valueh/2);
+	if ((isNaN(valuea)) || (isNaN(valueh))) {
+	document.getElementById("score3").innerHTML = 'Musisz wpisać liczby';
+	}
+	else {
+	document.getElementById("score3").innerHTML = ' Triangle Area is: ' + triangleArea1; 
+	}
+}
