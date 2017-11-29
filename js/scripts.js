7525e6c8ef4120e32f007beadbb9cf678b45e466
Grzegorz Koza <kozagrzegorz@interia.pl>
2017-11-29 01:33:56 +0100

    Add new function

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,34 @@
+function triangleArea(a, h) {
+	var result = (a * h / 2);
+	return result;
+}
 
 var a = 5;
 var h = 8;
-var triangleArea = (a*h/2);
-document.getElementById("score").innerHTML = 'Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea;
+document.getElementById("score").innerHTML = 'Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea(a, h);
 
 // triangle area with prompt
 
 
-	var valuea = prompt('Enter the base value');
-	var valueh = prompt('Enter the height value');	
-	/*var valuea = document.getElementById('value1').value;
-	var valueh = document.getElementById('value2').value;*/	
-	var triangleArea1 = (valuea*valueh/2);
+var a = prompt('Enter the base value');
+var h = prompt('Enter the height value');
 
-	if ((isNaN(valuea)) || (isNaN(valueh))) {
+if (isNaN(a) || isNaN(h)) {
 	document.getElementById("score2").innerHTML = 'Musisz wpisać liczby';
-	}
-
-	else {
-	document.getElementById("score2").innerHTML = ' Triangle Area is: ' + triangleArea1; 
-	}
+} 
+else {
+	document.getElementById("score2").innerHTML = ' Triangle Area is: ' + triangleArea(a, h);
+}
 
 
 // triangle area with two number area
 function area2() {
-	var valuea = document.getElementById('value1').value;
-	var valueh = document.getElementById('value2').value;	
-	var triangleArea1 = (valuea*valueh/2);
-	if ((isNaN(valuea)) || (isNaN(valueh))) {
-	document.getElementById("score3").innerHTML = 'Musisz wpisać liczby';
-	}
+	var a = document.getElementById("value1").value;
+	var h = document.getElementById("value2").value;
+	if (isNaN(a) || isNaN(h)) {
+		document.getElementById("score3").innerHTML = 'Musisz wpisać liczby';
+	} 
 	else {
-	document.getElementById("score3").innerHTML = ' Triangle Area is: ' + triangleArea1; 
+		document.getElementById("score3").innerHTML = ' Triangle Area is: ' + triangleArea(a, h);
 	}
 }
